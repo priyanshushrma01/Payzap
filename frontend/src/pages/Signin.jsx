dc200f1dd50d4909dd94b21ea146425f9e95c95b
Priyanshu Sharma <priyanshu.sh18@gmail.com>
2025-01-15 02:17:09 +0530

    fix: signin issue

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -5,11 +5,13 @@ import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 export const Signin = ()=>{
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
-
+    const navigate = useNavigate();
+    
     return <div className="bg-slate-300 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
             <div className="bg-white rounded-lg w-80 text-center p-2 px-4 h-max">
