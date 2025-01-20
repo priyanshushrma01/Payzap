fcc2efc87007056f9112e42a8cff284ae112486b
Priyanshu Sharma <priyanshu.sh18@gmail.com>
2025-01-20 20:06:15 +0530

    minor fix in db file

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,24 @@
-// backend/db.js
 const mongoose = require('mongoose');
-const dotenv = require('dotenv')
-dotenv.config();
 
-mongoose.connect(process.env.DATABASE_URL)
+// Load environment variables if not in production
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => {
         console.log('Connected to MongoDB');
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit process on connection failure
     });
 
-// Create a Schema for Users
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -20,43 +27,40 @@ const userSchema = new mongoose.Schema({
         trim: true,
         lowercase: true,
         minLength: 3,
-        maxLength: 30
+        maxLength: 30,
     },
     password: {
         type: String,
         required: true,
-        minLength: 6
+        minLength: 6,
     },
     firstName: {
         type: String,
         required: true,
         trim: true,
-        maxLength: 50
+        maxLength: 50,
     },
     lastName: {
         type: String,
         required: true,
         trim: true,
-        maxLength: 50
-    }
+        maxLength: 50,
+    },
 });
 
 const accountSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId, // Reference to User model
         ref: 'User',
-        required: true
+        required: true,
     },
     balance: {
         type: Number,
-        required: true
-    }
+        required: true,
+    },
 });
 
 const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User', userSchema);
 
-module.exports = {
-	User,
-  Account,
-};
+module.exports = { User, Account };
